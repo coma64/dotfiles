ce0513c39476c4488bc8ce92909761b56d70767d
coma64 <zaton.tristan@gmail.com>
2022-06-24 20:46:43 +0200

    Update xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,11 +4,14 @@ import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Ungrab (unGrab)
 import XMonad.Layout.ThreeColumns (ThreeCol(..))
-import XMonad.Hooks.EwmhDesktops (ewmh)
+import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.Loggers
 
 fillWorkspaces :: [String] -> Int -> [String]
-fillWorkspaces ws x = ws ++ (show <$> [l..l + x])
+fillWorkspaces ws x = ws ++ (show <$> [(l + 1)..x])
     where l = length ws
 
 -- If you change layout bindings be sure to use 'mod-shift-space' after
@@ -21,10 +24,11 @@ myLayout = tiled ||| Mirror tiled ||| Full
     ratio    = 1/2    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
 
+myTerminal = "alacritty"
 
 myConfig = def
     { modMask               = mod4Mask
-    , terminal              = "alacritty"
+    , terminal              = myTerminal
     , focusFollowsMouse     = False
     , normalBorderColor     = "#888888"
     , focusedBorderColor    = "#bed600"
@@ -32,10 +36,45 @@ myConfig = def
     , layoutHook            = myLayout
     }
     `additionalKeysP`
-    [ ("M-c", spawn "google-chrome-stable")
-    , ("M-s", unGrab *> spawn "scrot -s")
-    , ("M-m", spawn "spotify")
+    [ ("M-g", spawn "google-chrome-stable")
+    , ("M-m", unGrab *> spawn "scrot -s")
+    , ("M-s", spawn "spotify")
+    , ("M-c", kill)
+    , ("M-Q", spawn "shutdown now")
+    , ("M-<Return>", spawn myTerminal)
     ]
 
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+
 main :: IO ()
-main = xmonad . ewmh =<< xmobar myConfig
+main = xmonad
+     . ewmhFullscreen
+     . ewmh
+     . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
+     $ myConfig
